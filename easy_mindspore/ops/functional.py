c8ee0adb92e7969de2bcc483b161576f6f2a712f
lvyufeng <lvyufeng@cqu.edu.cn>
2022-07-19 00:50:38 +0800

    add ops.norm

--- easy_mindspore/ops/functional.py
+++ easy_mindspore/ops/functional.py
@@ -1,25 +1,176 @@
+import mindspore
 from mindspore import Tensor
-import mindspore.ops as P
+import mindspore.ops as ops
 from .custom import *
+from mindspore.ops._primitive_cache import _get_cache_prim
+inf = float('inf')
+
+def _check_dtype(d1, d2):
+    if mindspore.float32 in (d1, d2):
+        return mindspore.float32
+    if d1 == d2:
+        return d1
+    raise ValueError('dtype is not supported.')
+
+def dot(a, b):
+    res_dtype = _check_dtype(a.dtype, b.dtype)
+    ndim_a, ndim_b = a.ndim, b.ndim
+    if ndim_a == 0 or ndim_b == 0:
+        return ops.tensor_mul(a, b)
+    if ndim_a > 0 and ndim_b >= 2:
+        perm = ops.make_range(ndim_b)
+        perm = perm[:-2] + (perm[-1],) + (perm[-2],)
+        b = ops.transpose(b, perm)
+
+    if a.shape[-1] != b.shape[-1]:
+        raise ValueError('shapes are not aligned')
+    a_aligned = a.reshape(-1, a.shape[-1]).astype(mindspore.float32)
+    b_aligned = b.reshape(-1, b.shape[-1]).astype(mindspore.float32)
+
+    res = ops.matmul(a_aligned, b_aligned.T)
+    res = res.reshape(a.shape[:-1] + b.shape[:-1])
+
+    return res.astype(res_dtype)
+
+def sqrt(x):
+    return ops.sqrt(x.astype(mindspore.float32))
+
+def reciprocal(x):
+    if isinstance(x, Tensor):
+        reciprocal = _get_cache_prim(ops.Reciprocal)()
+        return reciprocal(x)
+    return 1/x
 
 # grad operations
 def get_grads():
     pass
 
-# math operations
-def sqrt(x):
-    sqrt_op = P.Sqrt()
-    if isinstance(x, Tensor):
-        return sqrt_op(x)
-    if isinstance(x, int) or isinstance(x, float):
-        return sqrt_op(P.ScalarToTensor()(x))
-
-# tensor operations
-def concat(inputs, axis=0):
-    return P.Concat(axis=axis)(inputs)
-
 def bmm(x, y, transpose_x=False, transpose_y=False):
-    return P.BatchMatMul(transpose_x, transpose_y)(x, y)
+    return _get_cache_prim(ops.BatchMatMul)(transpose_x, transpose_y)(x, y)
 
 def masked_fill_(inputs:Tensor, mask:Tensor, value:float):
-    return MaskedFill(value)(inputs, mask)
+    return ops.masked_fill(inputs, mask, value)
+
+def norm(x, ord=None, axis=None, keepdims=False):
+    # Immediately handle some default, simple, fast, and common cases.
+    if axis is None:
+        ndim = x.ndim
+        if ((ord is None) or
+            (ord in ('f', 'fro') and ndim == 2) or
+            (ord == 2 and ndim == 1)):
+
+            x = x.ravel()
+            sqnorm = dot(x, x)
+            ret = sqrt(sqnorm)
+            if keepdims:
+                ret = ret.reshape(ndim*[1])
+            return ret
+
+    # Normalize the `axis` argument to a tuple.
+    nd = x.ndim
+    if axis is None:
+        axis = tuple(range(nd))
+    elif not isinstance(axis, tuple):
+        try:
+            axis = int(axis)
+        except Exception as e:
+            raise TypeError("'axis' must be None, an integer or a tuple of integers") from e
+        axis = (axis,)
+
+    if len(axis) == 1:
+        if ord == inf:
+            return ops.abs(x).max(axis=axis, keepdims=keepdims)
+        elif ord == -inf:
+            return ops.abs(x).min(axis=axis, keepdims=keepdims)
+        elif ord == 0:
+            # Zero norm
+            return (x != 0).astype(x.dtype).sum(axis=axis, keepdims=keepdims)
+        elif ord == 1:
+            # special case for speedup
+            reduce_sum = _get_cache_prim(ops.ReduceSum)(keepdims)
+            return ops.reduce_sum(ops.abs(x), axis=axis)
+        elif ord is None or ord == 2:
+            # special case for speedup
+            conj = _get_cache_prim(ops.Conj)()
+            s = conj(x) * x
+            reduce_sum = _get_cache_prim(ops.ReduceSum)(keepdims)
+            return sqrt(reduce_sum(s, axis=axis))
+        # None of the str-type keywords for ord ('fro', 'nuc')
+        # are valid for vectors
+        elif isinstance(ord, str):
+            raise ValueError(f"Invalid norm order '{ord}' for vectors")
+        else:
+            absx = ops.abs(x)
+            absx **= ord
+            reduce_sum = _get_cache_prim(ops.ReduceSum)(keepdims)
+            ret = reduce_sum(absx, axis=axis)
+            ret **= reciprocal(ord)
+            if ops.isnan(ret):
+                return ops.zeros_like(ret)
+            return ret
+    elif len(axis) == 2:
+        row_axis, col_axis = axis
+        row_axis = normalize_axis_index(row_axis, nd)
+        col_axis = normalize_axis_index(col_axis, nd)
+        if row_axis == col_axis:
+            raise ValueError('Duplicate axes given.')
+        if ord == 2:
+            ret =  _multi_svd_norm(x, row_axis, col_axis, 'amax')
+        elif ord == -2:
+            ret = _multi_svd_norm(x, row_axis, col_axis, 'amin')
+        elif ord == 1:
+            if col_axis > row_axis:
+                col_axis -= 1
+            ret = ops.reduce_sum(abs(x), axis=row_axis).max(axis=col_axis)
+        elif ord == inf:
+            if row_axis > col_axis:
+                row_axis -= 1
+            ret = ops.reduce_sum(abs(x), axis=col_axis).max(axis=row_axis)
+        elif ord == -1:
+            if col_axis > row_axis:
+                col_axis -= 1
+            ret = ops.reduce_sum(abs(x), axis=row_axis).min(axis=col_axis)
+        elif ord == -inf:
+            if row_axis > col_axis:
+                row_axis -= 1
+            ret = ops.reduce_sum(abs(x), axis=col_axis).min(axis=row_axis)
+        elif ord in [None, 'fro', 'f']:
+            conj = _get_cache_prim(ops.Conj)()
+            ret = sqrt(ops.reduce_sum((conj(x) * x), axis=axis))
+        elif ord == 'nuc':
+            ret = _multi_svd_norm(x, row_axis, col_axis, sum)
+        else:
+            raise ValueError("Invalid norm order for matrices.")
+        if keepdims:
+            ret_shape = list(x.shape)
+            ret_shape[axis[0]] = 1
+            ret_shape[axis[1]] = 1
+            ret = ret.reshape(ret_shape)
+        return ret
+    else:
+        raise ValueError("Improper number of dimensions to norm.")
+
+def _multi_svd_norm(x, row_axis, col_axis, op):
+    y = moveaxis(x.astype(mindspore.float32), (row_axis, col_axis), (-2, -1))
+    if op == 'amax':
+        result = ops.svd(y, compute_uv=False).max(axis=-1)
+    elif op == 'amin':
+        result = ops.svd(y, compute_uv=False).min(axis=-1)
+    return result
+
+def normalize_axis_index(axis, ndim):
+    if axis >= 0 and axis < ndim:
+        return axis
+    elif axis < 0 and axis >= -ndim:
+        return ndim + axis
+    else:
+        raise ValueError('axis is out of range.')
+
+def moveaxis(x, source, destination):
+    perm = [i for i in range(x.ndim)]
+    for s, d in zip(source, destination):
+        tmp = perm[s]
+        perm[s] = perm[d]
+        perm[d] = tmp
+    perm = tuple(perm)
+    return ops.transpose(x, perm)
---
+++ tests/operators/test_norm.py
@@ -0,0 +1,151 @@
+import unittest
+import mindspore
+import numpy as np
+import mindspore.ops as ops
+from easy_mindspore.ops import norm, inf
+
+class TestNorm(unittest.TestCase):
+    def setUp(self):
+        self.a = ops.arange(9) - 4
+        self.b = self.a.reshape((3, 3))
+        self.c = mindspore.Tensor([[ 1, 2, 3],
+                                   [-1, 1, 4]], mindspore.int32)
+        self.m = ops.arange(8).reshape(2,2,2)
+
+    def test_norm_a(self):
+        out = norm(self.a)
+        out_np = np.linalg.norm(self.a.asnumpy())
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_b(self):
+        out = norm(self.b)
+        out_np = np.linalg.norm(self.b.asnumpy())
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_b_fro(self):
+        out = norm(self.b, 'fro')
+        out_np = np.linalg.norm(self.b.asnumpy(), 'fro')
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_a_inf(self):
+        out = norm(self.a, inf)
+        out_np = np.linalg.norm(self.a.asnumpy(), np.inf)
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_b_inf(self):
+        out = norm(self.b, inf)
+        out_np = np.linalg.norm(self.b.asnumpy(), np.inf)
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_a_neg_inf(self):
+        out = norm(self.a, -inf)
+        out_np = np.linalg.norm(self.a.asnumpy(), -np.inf)
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_b_neg_inf(self):
+        out = norm(self.b, -inf)
+        out_np = np.linalg.norm(self.b.asnumpy(), -np.inf)
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_a_1(self):
+        out = norm(self.a, 1)
+        out_np = np.linalg.norm(self.a.asnumpy(), 1)
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_b_1(self):
+        out = norm(self.b, 1)
+        out_np = np.linalg.norm(self.b.asnumpy(), 1)
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_a_neg_1(self):
+        out = norm(self.a, -1)
+        out_np = np.linalg.norm(self.a.asnumpy(), -1)
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_b_neg_1(self):
+        out = norm(self.b, -1)
+        out_np = np.linalg.norm(self.b.asnumpy(), -1)
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_a_2(self):
+        out = norm(self.a, 2)
+        out_np = np.linalg.norm(self.a.asnumpy(), 2)
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_b_2(self):
+        out = norm(self.b, 2)
+        out_np = np.linalg.norm(self.b.asnumpy(), 2)
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_a_neg_2(self):
+        out = norm(self.a, -2)
+        out_np = np.linalg.norm(self.a.asnumpy(), -2)
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_b_neg_2(self):
+        out = norm(self.b, -2)
+        out_np = np.linalg.norm(self.b.asnumpy(), -2)
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_a_3(self):
+        out = norm(self.a, 3)
+        out_np = np.linalg.norm(self.a.asnumpy(), 3)
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_a_neg_3(self):
+        out = norm(self.a, -3)
+        out_np = np.linalg.norm(self.a.asnumpy(), -3)
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_a_axis_0(self):
+        out = norm(self.c, axis=0)
+        out_np = np.linalg.norm(self.c.asnumpy(), axis=0)
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_a_axis_1(self):
+        out = norm(self.c, axis=1)
+        out_np = np.linalg.norm(self.c.asnumpy(), axis=1)
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_a_ord_1_axis_1(self):
+        out = norm(self.c, ord=1, axis=1)
+        out_np = np.linalg.norm(self.c.asnumpy(), ord=1, axis=1)
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_a_axis_1_2(self):
+        out = norm(self.m, axis=(1,2))
+        out_np = np.linalg.norm(self.m.asnumpy(), axis=(1,2))
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_a_slice_0(self):
+        out = norm(self.m[0:, :, :])
+        out_np = np.linalg.norm(self.m[0:, :, :].asnumpy())
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
+
+    def test_norm_a_slice_1(self):
+        out = norm(self.m[1:, :, :])
+        out_np = np.linalg.norm(self.m[1:, :, :].asnumpy())
+
+        assert np.allclose(out.asnumpy(), out_np, 1e-5, 1e-5)
